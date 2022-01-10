6410aedd52b96b647127222f57c73c33f3098c2d
matheusr1321 <matheusrbv99@gmail.com>
2022-01-10 08:35:27 -0300

    first commit

---
+++ build/CMakeFiles/mono_vo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mono_vo.dir/main.cpp.o"
+  "mono_vo"
+  "mono_vo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mono_vo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
